a633eaa4a0273432b042cb06c4a8cc9c074edca0
Montmorency <lamberh@tcd.ie>
2022-10-10 13:54:13 +0100

    Pattern patching empt column names.

--- IHP/IDE/SchemaDesigner/Compiler.hs
+++ IHP/IDE/SchemaDesigner/Compiler.hs
@@ -23,7 +23,8 @@ compileSql statements = statements
 
 compileStatement :: Statement -> Text
 compileStatement (StatementCreateTable CreateTable { name, columns, primaryKeyConstraint, constraints }) = "CREATE TABLE " <> compileIdentifier name <> " (\n" <> intercalate ",\n" (map (\col -> "    " <> compileColumn primaryKeyConstraint col) columns <> maybe [] ((:[]) . indent) (compilePrimaryKeyConstraint primaryKeyConstraint) <> map (indent . compileConstraint) constraints) <> "\n);"
-compileStatement (StatementCreateView CreateView {name, columns, query}) = "CREATE VIEW" <> compileIdentifier name <> " (\n" <> intercalate ",\n" (map (\col -> "    " <> compileViewColumn col) columns) <> "\n)" <> " as \n" <> query <> "\n;"
+compileStatement (StatementCreateView CreateView {name, [], query}) = "CREATE VIEW" <> compileIdentifier name <> " AS \n" <> query <> "\n;"
+compileStatement (StatementCreateView CreateView {name, columns, query}) = "CREATE VIEW" <> compileIdentifier name <> " (\n" <> intercalate ",\n" (map (\col -> "    " <> compileViewColumn col) columns) <> "\n)" <> " AS \n" <> query <> "\n;"
 compileStatement CreateEnumType { name, values } = "CREATE TYPE " <> compileIdentifier name <> " AS ENUM (" <> intercalate ", " (values |> map TextExpression |> map compileExpression) <> ");"
 compileStatement CreateExtension { name, ifNotExists } = "CREATE EXTENSION " <> (if ifNotExists then "IF NOT EXISTS " else "") <> compileIdentifier name <> ";"
 compileStatement AddConstraint { tableName, constraint = UniqueConstraint { name = Nothing, columnNames } } = "ALTER TABLE " <> compileIdentifier tableName <> " ADD UNIQUE (" <> intercalate ", " columnNames <> ")" <> ";"
